c8ca245755363d54df112e64f9471b4327bd8f0a
Suleman Ahmed <48311202+Suleman3015@users.noreply.github.com>
2020-09-13 16:40:08 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import CenterBody from "./components/centerBody";
 import Head from "./components/Head";
 import Cards from "./components/cards";
-
+import Footer from "./components/Footer";
+import Nav from "./components/naaav";
+import Ft from "./components/Footer2";
 function App() {
   return (
     <div>
+      <Nav />
       <Head />
       <CenterBody />
       <Cards />
+      <Footer />
     </div>
   );
 }
